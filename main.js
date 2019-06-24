0bb4b3132d580a747dcd5c7b946a594390535298
Mathias Chunnoo <mathiaschunnoo@28.193-69-171.fiber.lynet.no>
2019-06-24 19:44:46 +0200

    moved files to outer folder

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ express = require("express");
 app = express();
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname + "/client/plotting.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get("/*.html", function(req, res) {
-  res.sendFile(path.join(__dirname + "/client/" + url.parse(req.url, true).pathname));
+  res.sendFile(path.join(__dirname + "/" + url.parse(req.url, true).pathname));
 });
 app.get("/*.css", function(req, res) {
-  res.sendFile(path.join(__dirname + "/client/" + url.parse(req.url, true).pathname));
+  res.sendFile(path.join(__dirname + "/" + url.parse(req.url, true).pathname));
 });
 app.get("/*.js", function(req, res) {
-  res.sendFile(path.join(__dirname + "/client/" + url.parse(req.url, true).pathname));
+  res.sendFile(path.join(__dirname + "/" + url.parse(req.url, true).pathname));
 });
 
 app.listen(8080);
